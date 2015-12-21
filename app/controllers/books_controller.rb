6bff609a17d60d231a90c507e684d6e421d029ba
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-12-21 23:25:20 +0900

    add POST /books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def create
+    book = Book.new(book_params)
+
+    if book.valid?
+      book.save
+
+      render json: {book_id: book.id}
+    else
+      render json: {errors: book.errors.full_messages.join(' ')}, status: :bad_request
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :price)
+  end
+end
---
+++ spec/requests/books_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'book' do
+  before(:all) do
+    Book.destroy_all
+  end
+
+  example 'success' do
+    params = {
+      book: {
+        title: 'RSpec books',
+        price: 1000
+      }
+    }
+
+    post '/books', params
+
+    expect(status).to eq(200)
+
+    json_body = JSON.parse(response.body)
+    expect(json_body['book_id']).to eq(Book.last.id)
+  end
+
+  example 'invalid' do
+    params = {
+      book: {
+        title: nil,
+        price: 1000
+      }
+    }
+
+    post '/books', params
+
+    expect(status).to eq(400)
+
+    json_body = JSON.parse(response.body)
+    expect(json_body['errors']).to eq("Title can't be blank")
+  end
+end
